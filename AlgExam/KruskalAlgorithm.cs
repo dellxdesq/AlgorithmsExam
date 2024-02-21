3b7f9024a184b1eb58a155510ac820370e36ffc4
dellxdesq <nikita673551@gmail.com>
2024-02-21 15:36:04 +0300

    new

---
+++ AlgExam/KruskalAlgorithm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgExam
+{
+    public class KruskalAlgorithm
+    {
+        // Класс, представляющий ребро графа
+        public class Edge
+        {
+            public int Source { get; set; } // Начальная вершина ребра
+            public int Destination { get; set; } // Конечная вершина ребра
+            public int Weight { get; set; } // Вес ребра
+        }
+
+        private int V; // Количество вершин в графе
+        private List<Edge> edges; // Список рёбер графа
+
+        public KruskalAlgorithm(int v)
+        {
+            V = v; // Инициализация количества вершин
+            edges = new List<Edge>(); // Инициализация списка рёбер
+        }
+
+        // Метод для добавления ребра в список рёбер графа
+        public void AddEdge(int src, int dest, int weight)
+        {
+            edges.Add(new Edge { Source = src, Destination = dest, Weight = weight });
+        }
+
+        // Метод для поиска минимального остовного дерева алгоритмом Крускала
+        public void KruskalMST()
+        {
+            // Сортировка рёбер по весу
+            edges = edges.OrderBy(edge => edge.Weight).ToList();
+
+            // Создание массива для хранения корней подмножеств
+            var parent = Enumerable.Range(0, V).ToArray();
+
+            // Функция для нахождения корня подмножества, к которому принадлежит вершина i
+            int Find(int i) => parent[i] == i ? i : (parent[i] = Find(parent[i]));
+
+            // Проход по всем рёбрам и добавление их в остовное дерево, если они не образуют цикл
+            foreach (var edge in edges)
+            {
+                int x = Find(edge.Source); // Находим корень вершины-источника ребра
+                int y = Find(edge.Destination); // Находим корень вершины-приёмника ребра
+                if (x != y) // Если вершины принадлежат разным подмножествам, то добавляем ребро в остовное дерево
+                {
+                    Console.WriteLine($"{edge.Source} -- {edge.Destination} == {edge.Weight}");
+                    parent[x] = y; // Объединяем подмножества вершин
+                }
+            }
+        }
+    }
+}
---
+++ AlgExam/PrimAlgorithm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class PrimAlgorithm
+{
+    private int V;
+    private List<List<(int vertex, int weight)>> graph; // Изменили тип данных на кортеж (вершина, вес)
+
+    public PrimAlgorithm(int v)
+    {
+        V = v;
+        graph = new List<List<(int vertex, int weight)>>(V);
+        for (int i = 0; i < V; i++)
+            graph.Add(new List<(int vertex, int weight)>());
+    }
+
+    public void AddEdge(int src, int dest, int weight)
+    {
+        graph[src].Add((dest, weight)); // Добавляем вес ребра и конечную вершину в список смежности начальной вершины
+        graph[dest].Add((src, weight)); // Добавляем вес ребра и начальную вершину в список смежности конечной вершины
+    }
+
+    public void PrimMST()
+    {
+        bool[] mstSet = new bool[V];
+        int[] parent = new int[V];
+        int[] key = new int[V];
+
+        for (int i = 0; i < V; i++)
+        {
+            key[i] = int.MaxValue;
+            mstSet[i] = false;
+        }
+
+        key[0] = 0;
+        parent[0] = -1;
+
+        for (int count = 0; count < V - 1; count++)
+        {
+            int u = MinKey(key, mstSet);
+            mstSet[u] = true;
+
+            foreach (var edge in graph[u])
+            {
+                int v = edge.vertex;
+                int weight = edge.weight;
+                if (!mstSet[v] && weight < key[v])
+                {
+                    parent[v] = u;
+                    key[v] = weight;
+                }
+            }
+        }
+
+        Console.WriteLine("Edge \tWeight");
+        for (int i = 1; i < V; i++)
+            Console.WriteLine($"{parent[i]} - {i}\t{key[i]}");
+    }
+
+    private int MinKey(int[] key, bool[] mstSet)
+    {
+        int min = int.MaxValue;
+        int minIndex = -1;
+
+        for (int v = 0; v < V; v++)
+        {
+            if (mstSet[v] == false && key[v] < min)
+            {
+                min = key[v];
+                minIndex = v;
+            }
+        }
+
+        return minIndex;
+    }
+}
+
+
